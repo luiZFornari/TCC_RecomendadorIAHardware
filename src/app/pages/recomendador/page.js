f84f99bfdb03c6b1d2d6b8fa0be2bf16b3a18b71
Luiz Felipe Fornari <luizfelipefornari@GMAIL.COM>
2024-09-26 17:25:41 -0300

    update resultado

--- src/app/pages/recomendador/page.js
+++ src/app/pages/recomendador/page.js
@@ -85,8 +85,6 @@ export default function Recomendador() {
       localStorage.setItem("historicoConfiguracoes", JSON.stringify(historico));
       localStorage.setItem("apiResponse", JSON.stringify([data]));
       console.log(data);
-
-      router.push("/pages/resultado");
     } catch (error) {
       console.error("Erro ao chamar a API:", error);
       alert(
